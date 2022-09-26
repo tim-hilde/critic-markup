628820d7a1d0ffc61ae5b871578abe5445cc7fc1
tim-hilde <tim.hildebrandt@me.com>
2022-09-26 22:07:33 +0200

    add regex and cases

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import {Plugin} from 'obsidian';
 
-import {additionRegExp, deletionRegExp, substitutionRegExp, commentRegExp, highlightRegExp} from 'regExps';
+import {criticRegExp, simpleMarkupRegExp, substitutionRegExp} from 'regExps';
 
+const testString = "Addition {++ ++} Deletion {-- --} Substitution {~~ ~> ~~}Comment {>> <<}Highlight {== ==}{>> <<}";
+
+const citeRegExp =
+  /(?<=^|[.;\s-[])(?:(\[)([^[\]@\n\r]*)((?:@[^@\s[\];,'"|]+(?:; *)?)+)([^;[\]]*)(\])|(@[^@\s[\];,'"|]+)(?:( *)(\[)([^[\]]+)(\]))?)/g;
+
+const test = "[@ropohlEthikUndTechnikbewertung1996] sdf [@ropohlEthikUndTechnikbewertung1996; @weberMEESTARModellZur2015]"
 
 
 export default class CriticMarkup extends Plugin {
@@ -9,8 +15,44 @@ export default class CriticMarkup extends Plugin {
 	async onload() {
 		console.log("loading critic-markup");
 		
+		let match;
+
+		while ((match = criticRegExp.exec(testString))) {
+			for (let i = 1; i <= 5; i++) {
+				switch (i) {
+					case 1:
+					case 2:
+					case 4:
+					case 5:
+						if (match[i]) {
+							const simpleMarkup = match[i];
+							let m2;
+							while ((m2 = simpleMarkupRegExp.exec(simpleMarkup))) {
+								// m2[1] -> pre
+								// m2[2] -> text
+								// m2[3] -> post
+							}
+						}
+						continue;
+					case 3:
+						if (match[i]) {
+							const substitution = match[i];
+							let m2;
+							while ((m2 = substitutionRegExp.exec(substitution))) {
+								// m2[1] -> markup
+								// m2[2] -> deletion
+								// m2[3] -> markup
+								// m2[4] -> substitute
+								// m2[5] -> marktup
+							}
+						}
+				}
+			}
+		}
 
 	}
+
+
 
 	onunload() {
 		console.log("unloading critic-markup")
--- regExps.ts
+++ regExps.ts
@@ -1,11 +1,14 @@
-export const additionRegExp = /(?:({\+\+)([^+]*)(\+\+}))/g;
-// markup, addition, markup
-export const deletionRegExp = /(?:({--)([^-]*)(--}))/g;
-// markup, deletion, markup
-export const substitutionRegExp = /(?:({~~)([^~]*)(~>)([^~]*)(~~}))/g;
-// markup, delete, markup, substitute, marktup
-export const commentRegExp = / (?:({>>)([^<]*)(<<)})/g;
-// markup, comment, markup
-export const highlightRegExp = /(?:({==)([^=]*)(==}))/g;
-// marktup, highlight, markup
+export const criticRegExp = /(?:({\+\+[^+]*\+\+})|({--[^-]*--})|({~~[^~]*~>[^~]*~~})|({>>[^<]*<<})|({==[^=]*==}))/g;
+// 1 -> addition
+// 2 -> deletion
+// 3 -> substitution
+// 4 -> comment
+// 5 -> highlight
 
+export const substitutionRegExp = /(?:({~~)([^~]*)(~>)([^~]*)(~~}))/g;
+// markup, delete, markup, substitute, markup
+
+export const simpleMarkupRegExp = /(?:({\+\+|{--|{>>|{==)([^\+\-\<\=]*)(\+\+}|--}|<<}|==}))/g;
+// 1 -> pre
+// 2 -> markup
+// 3 -> post
