70f72c570f4e838a3846557afeba625fd51503a4
tim-hilde <tim.hildebrandt@me.com>
2022-09-26 13:21:28 +0200

    Add regexp

---
+++ regExps.ts
@@ -0,0 +1,10 @@
+export const additionRegExp = /(?:({\+\+)([^+]*)(\+\+}))/g;
+// markup, addition, markup
+export const deletionRegExp = /(?:({--)([^-]*)(--}))/g;
+// markup, deletion, markup
+export const substitutionRegExp = /(?:({~~)([^~]*)(~>)([^~]*)(~~}))/g;
+// markup, delete, markup, substitute, marktup
+export const commentRegExp = / (?:({>>)([^<]*)(<<)})/g;
+// markup, comment, markup
+export const highlightRegExp = /(?:({==)([^=]*)(==}))/g;
+// marktup, highlight, markup
